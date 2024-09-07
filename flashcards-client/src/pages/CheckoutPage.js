e84ea6c41c14102b4fcd3c7150ff4f236b09e39d
manahil <120031852+manahiliqbal@users.noreply.github.com>
2024-09-08 02:24:19 +0500

    render

--- flashcards-client/src/pages/CheckoutPage.js
+++ flashcards-client/src/pages/CheckoutPage.js
@@ -10,7 +10,7 @@ const CheckoutForm = () => {
   const [clientSecret, setClientSecret] = useState('');
 
   useEffect(() => {
-    fetch('http://localhost:5000/create-payment-intent', {
+    fetch('https://cardify-7j28.onrender.com/create-payment-intent', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
